003c08b46138035bbf1cf7c9151a9cc3142a62ed
Greg Holmes <greg.holmes@tru.id>
2022-10-06 13:13:33 +0100

    Added users dependant on the sandbox conditions

--- src/seeders/20220607152032-seed-user.js
+++ src/seeders/20220607152032-seed-user.js
@@ -14,8 +14,17 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [{
       firstName: 'Valid Phone',
       lastName: 'Numer',
-      phone: '+447000000000',
+      phone: '+447000000070',
       email: 'valid.phone.number@example.com',
+      // This is the SHA256 hash for value of `password`
+      password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=',
+      createdAt: new Date().toDateString(),
+      updatedAt: new Date().toDateString()
+    }, {
+      firstName: 'Valid Phone',
+      lastName: 'Number SIM changed',
+      phone: '+447000000008',
+      email: 'valid.phone.number.sim.changed@example.com',
       // This is the SHA256 hash for value of `password`
       password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=',
       createdAt: new Date().toDateString(),
